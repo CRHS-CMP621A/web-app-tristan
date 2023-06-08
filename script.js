9aa16335725779dbaf447aad4a09078f62678877
TristanMacken <tristanmacken@gmail.com>
2023-06-07 21:20:28 -0300

    Wednesday Night

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let AirBnbMarkers = []
 let HotelMarkers = []
 
 let range = 500;
+let coords= []
 
 let html;
 
@@ -80,6 +81,17 @@ class Hotel extends Location {
 
 }
 
+// function closeHotel(lat, lng) {
+
+//     Location1 = new Hotel([46.307204, -63.166118], 145325423, "name1", 4.97);
+//     Location2 = new Hotel([46.310287, -63.112217], 543262464, "name2", 3.24);
+//     Location3 = new Hotel([46.253460, -63.064152], 345634623, "name3", 5);
+
+//     HotelsList.push(Location1);
+//     HotelsList.push(Location2);
+//     HotelsList.push(Location3);
+
+// }
 
 async function closeHotel(lat, lng) {
 
@@ -87,7 +99,9 @@ async function closeHotel(lat, lng) {
     const options = {
         method: 'GET',
         headers: {
-            'X-RapidAPI-Key': '8e7df8cbd5msh829b9ca492c6cc9p166913jsnf7983ffebe22',
+            'X-RapidAPI-Key': '1fe1aa17admsh15038d1042c3005p19e50ejsn353d796658de',
+            // 'X-RapidAPI-Key': '90bdbd523cmsh0ed8ac001717b0cp1c45d5jsn4dd183fbf3c9',
+            // 'X-RapidAPI-Key': '8e7df8cbd5msh829b9ca492c6cc9p166913jsnf7983ffebe22',
             // 'X-RapidAPI-Key': '20b4f70deemsh2516ad2619539b1p13c0c9jsn0d4d91bd427a',
             'X-RapidAPI-Host': 'tripadvisor16.p.rapidapi.com'
         }
@@ -117,7 +131,9 @@ async function HotelDetails(id) {
     const options = {
         method: 'GET',
         headers: {
-            'X-RapidAPI-Key': '8e7df8cbd5msh829b9ca492c6cc9p166913jsnf7983ffebe22',
+            'X-RapidAPI-Key': '1fe1aa17admsh15038d1042c3005p19e50ejsn353d796658de',
+            // 'X-RapidAPI-Key': '90bdbd523cmsh0ed8ac001717b0cp1c45d5jsn4dd183fbf3c9',
+            // 'X-RapidAPI-Key': '8e7df8cbd5msh829b9ca492c6cc9p166913jsnf7983ffebe22',
             // 'X-RapidAPI-Key': '20b4f70deemsh2516ad2619539b1p13c0c9jsn0d4d91bd427a',
             'X-RapidAPI-Host': 'tripadvisor16.p.rapidapi.com'
         }
@@ -136,6 +152,10 @@ async function HotelDetails(id) {
             Location = new Hotel([lat,lng], id, name, rating);
 
             HotelsList.push(Location);
+
+            marker = L.marker(Location.coords).bindPopup(Location.name);
+        
+            HotelMarkers.push(marker);
 
     } catch (error) {
         console.error(error);
@@ -162,13 +182,13 @@ async function closeListings(lat, lng) {
 
         console.log(result);
 
-        for (let i = 0; i < result.results.length; i++){
+        for (let i = 0; i <= result.results.length; i++){
            
             let id = result['results'][i]['airbnb_id'];
             // console.log(id)
             listingDetails(id);
 
-        }
+        } 
 
 
     } catch (error) {
@@ -204,7 +224,10 @@ try {
 
     AirBnbsList.push(Location);
 
-    AirBnbMarkers.push(L.marker(Location.coords).bindPopup(Location.name));
+    marker = L.marker(Location.coords).bindPopup(Location.name);
+        
+    AirBnbMarkers.push(marker);
+
 
 } catch (error) {
 	// console.error(error);
@@ -212,80 +235,64 @@ try {
 
 }
 
+function GenerateMap(coords) {
+
+    //Layer Groups
+    var AirBnbs = L.layerGroup(AirBnbMarkers);
+    var Hotels = L.layerGroup(HotelMarkers);
+
+    //Set map view near your coords
+    var map = L.map('map', {
+        center: coords,
+        zoom: 13,
+        layers: [AirBnbs, Hotels]
+    });
+
+    //Generate Map
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    var baseMaps = {};
+
+    var overlayMaps = {
+        "AirBnbs": AirBnbs,
+        "Hotels": Hotels
+    };
+
+    var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
+
+    //Set maker at your coords
+    L.marker(coords).addTo(map)
+        .bindPopup('Your Current Location')
+        .openPopup();
+
+    //Set radius cirlce
+    var SearchArea = L.circle(coords, {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0,
+        radius: range*1000
+    }).addTo(map);
+    
+}
+
 navigator.geolocation.getCurrentPosition(
 
-    function (position) {
+    async function (position) {
 
         //Find Latitude & Longitude
         const latitude = position.coords.latitude;
         const longitude = position.coords.longitude;
 
-        closeListings(latitude, longitude);
-        closeHotel(latitude, longitude);
+        await closeListings(latitude, longitude);
+        await closeHotel(latitude, longitude);
+        console.log("test");
 
         //Make coords array
-        const coords = [latitude, longitude]
+        coords = [latitude, longitude]
 
-        //Make Hotel markers
-        for (let hotel of HotelsList) {
-
-            let marker = L.marker(hotel.coords).bindPopup(hotel.name);
-        
-            console.log(hotel.name);
-            HotelMarkers.push(marker);
-
-        }
-
-        //Make Airbnb markers
-        for (let airbnb of AirBnbsList) {
-
-            let marker = L.marker(airbnb.coords).bindPopup(airbnb.name);
-
-            console.log(airbnb.name);
-            AirBnbMarkers.push(marker);
-
-        }
-
-        //Layer Groups
-        var AirBnbs = L.layerGroup(AirBnbMarkers);
-        var Hotels = L.layerGroup(HotelMarkers);
-
-        //Set map view near your coords
-        var map = L.map('map', {
-            center: coords,
-            zoom: 13,
-            layers: [AirBnbs, Hotels]
-        });
-
-        //Generate Map
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
-
-        var baseMaps = {};
-
-        var overlayMaps = {
-            "AirBnbs": AirBnbs,
-            "Hotels": Hotels
-        };
-
-        var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
-
-        //Set maker at your coords
-        L.marker(coords).addTo(map)
-            .bindPopup('Your Current Location')
-            .openPopup();
-
-        //Set radius cirlce
-        var SearchArea = L.circle(coords, {
-            color: 'red',
-            fillColor: '#f03',
-            fillOpacity: 0,
-            radius: range*1000
-        }).addTo(map);
-
-        //Display google maps link in console
-        console.log("https://www.google.com/maps/@"+latitude+","+longitude+",15z");
+        // GenerateMap(coords);
         
     },
     function () {
@@ -296,18 +303,10 @@ navigator.geolocation.getCurrentPosition(
 
 );
 
-// document.querySelector(".button").addEventListener('click', function() {
-
-//     range = Number(document.querySelector(".range").value);
-
-//     if (radius = "" || range <= 0) {
-
-//         range = 500;
-
-//     }
-//     console.log(range);
-
-//     SearchArea.bindPopup("I am a circle.");
+document.querySelector(".button").addEventListener('click', function() {
 
 
-// })
+    GenerateMap(coords);
+
+
+})
